a576da4e5d6c8d33367dda5b01930460a3a0f886
louis-tamagny <louis.tamagny@gmail.com>
2024-03-20 17:05:49 +0100

    exercise 9.9 done
    -fork and clone frontend
    -add cors header

--- patientor/backend/index.ts
+++ patientor/backend/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
 const app = express();
 
+app.use((_req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  next();
+});
+
 app.get("/api/ping", (_req, res) => {
   res.status(200).send("pong");
 });
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
